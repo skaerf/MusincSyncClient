84014794c083499c7748a257367fcd5169101314
Lawrence Harrison <lawrence.harrison@outlook.com>
2022-12-08 16:23:39 +0000

    Initial Commit

---
+++ MusincClient2/MusincClient2App.swift
@@ -0,0 +1,17 @@
+//
+//  MusincClient2App.swift
+//  MusincClient2
+//
+//  Created by Lawrence Harrison on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusincClient2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusincClient2UITests/MusincClient2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MusincClient2UITestsLaunchTests.swift
+//  MusincClient2UITests
+//
+//  Created by Lawrence Harrison on 08/12/2022.
+//
+
+import XCTest
+
+final class MusincClient2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
